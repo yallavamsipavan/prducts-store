30e3630c665d14201297486451dbcfe3248f3a62
Vamsi Pavan <21131a1259@gvpce.ac.in>
2025-06-05 18:12:08 +0530

    hiding personal IDs

--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,17 @@ import express, { json } from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
-// import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// const __dirname = path.resolve();
-
 app.use(express.json());
 app.use("/api/products", productRoutes);
 
-// if(process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname, "/frontend/dist")));
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-//     });
-// }
 
 app.listen(PORT, () => {
     connectDB();
     console.log("Server started at http://localhost:" + PORT);
 });
-
-// password = vo4x0u6KpG7b3bXy
